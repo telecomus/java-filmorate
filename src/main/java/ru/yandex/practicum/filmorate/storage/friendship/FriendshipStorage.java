9c278f38b747ecfc11590d65c259b7f1269a4f64
Anton <telecomus@mail.ru>
2024-12-31 18:53:42 +0600

    fix code style

--- src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 public interface FriendshipStorage {
     void addFriend(int userId, int friendId);
+
     void removeFriend(int userId, int friendId);
+
     Set<Integer> getFriends(int userId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/like/FilmLikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/FilmLikeStorage.java
@@ -4,7 +4,10 @@ import java.util.Set;
 
 public interface FilmLikeStorage {
     void addLike(int filmId, int userId);
+
     void removeLike(int filmId, int userId);
+
     Set<Integer> getLikes(int filmId);
+
     int getLikesCount(int filmId);
 }
